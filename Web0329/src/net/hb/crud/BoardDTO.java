75158e2d19bbaab172429f42a91612108e2bcdc6
LeeJaeJune <LeeJaeJune@211.184.43.240>
2021-04-01 11:49:54 +0900

    오전 11:48 first test

---
+++ Web0329/src/net/hb/crud/BoardDTO.java
@@ -0,0 +1,13 @@
+package net.hb.crud;
+
+public class BoardDTO {
+	private String title;
+	
+	public String getTitle() {
+		return title;
+	}//end
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}//end
+}
